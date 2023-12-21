764ef2aaf7e3ef3bac619381fe8c201a2eb50527
lazare1999 <47102192+lazare1999@users.noreply.github.com>
2023-12-21 10:33:05 +0000

    Bugfix

--- feed.py
+++ feed.py
@@ -37,6 +37,6 @@ for item in yaml_data['item']:
     })
 
 output_tree = xml_tree.ElementTree(rss_element)
-output_tree.write('poscast.xml', encoding='UTF-8', xml_declaration=True)
+output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
 
  
